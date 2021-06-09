42fa2b4970d7d5caddf405393bcb9f25a46331df
Facundo <facufas1401@gmail.com>
2021-06-09 12:31:52 -0300

    Creando y editando pagina home y login

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 // import "bootstrap/dist/css/bootstrap.css";
 // import "font-awesome/css/font-awesome.css";
-import "./index.css";
+// import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
