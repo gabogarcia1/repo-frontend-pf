b3137c0b2b57fab336fa00a0593c4fe0d5406ce9
Rekke-uk <mazcm2013@gmail.com>
2021-06-15 21:52:18 -0300

    tabla funcional c3 con hooks

---
+++ src/App.js
@@ -0,0 +1,354 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap";
+import 'font-awesome/css/font-awesome.css'
+
+const data = [
+  { expediente: 1, nombreyapellido: "Tom Hiddleston ", curso: "3°Año", domicilio: "Ecuador 4036", contacto: "3816561429", dni: "44372965" },
+  { expediente: 2, nombreyapellido: "Gugu Mbatha-Raw", curso: "1°Año", domicilio: "Paraguay 4036", contacto: "3816561430", dni: "44372966" },
+  { expediente: 3, nombreyapellido: "Wunmi Mosaku", curso: "2°Año", domicilio: "Colombia 4036", contacto: "3816561431", dni: "44372967" },
+  { expediente: 4, nombreyapellido: "Eugene Cordero", curso: "1°Año", domicilio: "Peru 4036", contacto: "3816561432", dni: "44372968" },
+  { expediente: 5, nombreyapellido: "Tara Strong", curso: "3°Año", domicilio: "Chile 4036", contacto: "3816561433", dni: "44372969" },
+  { expediente: 6, nombreyapellido: "Owen Wilson", curso: "4°Año", domicilio: "Brasil 4036", contacto: "3816561434", dni: "44372970" },
+];
+
+class App extends React.Component {
+  state = {
+    data: data,
+    modalActualizar: false,
+    modalInsertar: false,
+    form: {
+      expediente: "",
+      nombreyapellido: "",
+      curso: "",
+    },
+  };
+
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
+
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
+
+  mostrarModalInsertar = () => {
+    this.setState({
+      modalInsertar: true,
+    });
+  };
+
+  cerrarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
+
+  editar = (dato) => {
+    var contador = 0;
+    var arreglo = this.state.data;
+    arreglo.map((registro) => {
+      if (dato.expediente == registro.expediente) {
+        arreglo[contador].nombreyapellido = dato.nombreyapellido;
+        arreglo[contador].curso = dato.curso;
+      }
+      contador++;
+    });
+    this.setState({ data: arreglo, modalActualizar: false });
+  };
+
+  eliminar = (dato) => {
+    var opcion = window.confirm("Estás Seguro que deseas Dar de Baja al Alumno " + dato.expediente);
+    if (opcion == true) {
+      var contador = 0;
+      var arreglo = this.state.data;
+      arreglo.map((registro) => {
+        if (dato.expediente == registro.expediente) {
+          arreglo.splice(contador, 1);
+        }
+        contador++;
+      });
+      this.setState({ data: arreglo, modalActualizar: false });
+    }
+  };
+
+  insertar = () => {
+    var valorNuevo = { ...this.state.form };
+    valorNuevo.expediente = this.state.data.length + 1;
+    var lista = this.state.data;
+    lista.push(valorNuevo);
+    this.setState({ modalInsertar: false, data: lista });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  render() {
+
+    return (
+      <>
+        <Container>
+          <br />
+          <Button color="success" onClick={() => this.mostrarModalInsertar()}>Crear</Button>
+          <br />
+          <br />
+          <Table>
+            <thead>
+              <tr>
+                <th>N° de Expediente</th>
+                <th>Nombre y Apellido</th>
+                <th>Curso</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {this.state.data.map((dato) => (
+                <tr key={dato.expediente}>
+                  <td>{dato.expediente}</td>
+                  <td>{dato.nombreyapellido}</td>
+                  <td>{dato.curso}</td>
+                  <td>
+                    <Button color="info" onClick={() => this.mostrarModalActualizar(dato)}><i class="fa fa-search-plus" aria-hidden="true"></i></Button>{" "}
+                    <Button
+                      color="secondary npm "
+                      onClick={() => this.mostrarModalActualizar(dato)}
+                    >
+                      <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    </Button>{" "}
+                    <Button color="danger" onClick={() => this.eliminar(dato)}><i class="fa fa-trash" aria-hidden="true"></i></Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+
+{/* Modal de los datos de los Alumnos */}
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+            <div><h3>Editar Registro</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>
+                N° de Expediente:
+              </label>
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.expediente}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Nombre y Apellido:
+              </label>
+              <input
+                className="form-control"
+                name="nombreyapellido"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.nombreyapellido}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Curso:
+              </label>
+              <input
+                className="form-control"
+                name="curso"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.curso}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Domicilio:
+              </label>
+              <input
+                className="form-control"
+                name="domicilio"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.domicilio}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Contacto:
+              </label>
+              <input
+                className="form-control"
+                name="contacto"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.contacto}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                DNI:
+              </label>
+              <input
+                className="form-control"
+                name="dni"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.dni}
+              />
+            </FormGroup>
+
+          </ModalBody>
+
+          <ModalFooter>
+
+            <Button
+              color="primary"
+              onClick={() => this.editar(this.state.form)}
+            >
+              Editar
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => this.cerrarModalActualizar()}
+            >
+              Cancelar
+            </Button>
+
+          </ModalFooter>
+
+        </Modal>
+
+
+{/* Modal para crear un nuevo Alumno */}
+        <Modal isOpen={this.state.modalInsertar}>
+
+          <ModalHeader>
+            <div><h3>Nuevo Perfil de Alumno</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>
+                N°de Expediente:
+              </label>
+
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.data.length + 1}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Nombre y Apellido:
+              </label>
+              <input
+                className="form-control"
+                name="nombreyapellido"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Curso:
+              </label>
+              <input
+                className="form-control"
+                name="curso"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Domicilio:
+              </label>
+              <input
+                className="form-control"
+                name="domicilio"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Contacto:
+              </label>
+              <input
+                className="form-control"
+                name="contacto"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                DNI:
+              </label>
+              <input
+                className="form-control"
+                name="dni"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+          </ModalBody>
+
+          <ModalFooter>
+
+            <Button
+              color="success"
+              onClick={() => this.insertar()}
+            >
+              Crear
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.cerrarModalInsertar()}
+            >
+              Cancelar
+            </Button>
+
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
+}
+export default App;
--- src/pages/ListaAlumnos.jsx
+++ src/pages/ListaAlumnos.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
 import "../css/listaDeAlumnos.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,16 +6,54 @@ import "font-awesome/css/font-awesome.css";
 
 const ListaAlumnos = () => {
   const dataPaises = [
-    { numerodeexpediente: 1, nombre: "Josh", minutos: 241 },
-    { numerodeexpediente: 2, nombre: "Josh", minutos: 225 },
-    { numerodeexpediente: 3, nombre: "Josh", minutos: 216 },
-    { numerodeexpediente: 4, nombre: "Josh", minutos: 216 },
-    { numerodeexpediente: 5, nombre: "Josh", minutos: 207 },
-    { numerodeexpediente: 6, nombre: "Josh", minutos: 195 },
-    { numerodeexpediente: 7, nombre: "Josh", minutos: 191 },
-    { numerodeexpediente: 8, nombre: "Josh", minutos: 190 },
-    { numerodeexpediente: 9, nombre: "Josh", minutos: 190 },
-    { numerodeexpediente: 10, nombre: "Josh", minutos: 186 },
+    {
+      expediente: 1,
+      nombreyapellido: "Tom Hiddleston ",
+      curso: "3° Año",
+      domicilio: "Ecuador 4036",
+      contacto: "3816561429",
+      dni: "44372965",
+    },
+    {
+      expediente: 2,
+      nombreyapellido: "Gugu Mbatha-Raw",
+      curso: "1° Año",
+      domicilio: "Paraguay 4036",
+      contacto: "3816561430",
+      dni: "44372966",
+    },
+    {
+      expediente: 3,
+      nombreyapellido: "Wunmi Mosaku",
+      curso: "2° Año",
+      domicilio: "Colombia 4036",
+      contacto: "3816561431",
+      dni: "44372967",
+    },
+    {
+      expediente: 4,
+      nombreyapellido: "Eugene Cordero",
+      curso: "1° Año",
+      domicilio: "Peru 4036",
+      contacto: "3816561432",
+      dni: "44372968",
+    },
+    {
+      expediente: 5,
+      nombreyapellido: "Tara Strong",
+      curso: "3° Año",
+      domicilio: "Chile 4036",
+      contacto: "3816561433",
+      dni: "44372969",
+    },
+    {
+      expediente: 6,
+      nombreyapellido: "Owen Wilson",
+      curso: "4° Año",
+      domicilio: "Brasil 4036",
+      contacto: "3816561434",
+      dni: "44372970",
+    },
   ];
 
   const [data, setData] = useState(dataPaises);
@@ -23,20 +61,20 @@ const ListaAlumnos = () => {
   const [modalEliminar, setModalEliminar] = useState(false);
   const [modalInsertar, setModalInsertar] = useState(false);
 
-  const [paisSeleccionado, setPaisSeleccionado] = useState({
-    numerodeexpediente: "",
-    nombre: "",
-    minutos: "",
+  const [AlumnoSeleccionado, setAlumnoSeleccionado] = useState({
+    expediente: "",
+    nombreyapellido: "",
+    curso: "",
   });
 
   const seleccionarPais = (elemento, caso) => {
-    setPaisSeleccionado(elemento);
+    setAlumnoSeleccionado(elemento);
     caso === "Editar" ? setModalEditar(true) : setModalEliminar(true);
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setPaisSeleccionado((prevState) => ({
+    setAlumnoSeleccionado((prevState) => ({
       ...prevState,
       [name]: value,
     }));
@@ -45,9 +83,9 @@ const ListaAlumnos = () => {
   const editar = () => {
     var dataNueva = data;
     dataNueva.map((pais) => {
-      if (pais.numerodeexpediente === paisSeleccionado.numerodeexpediente) {
-        pais.minutos = paisSeleccionado.minutos;
-        pais.nombre = paisSeleccionado.nombre;
+      if (pais.expediente === AlumnoSeleccionado.expediente) {
+        pais.curso = AlumnoSeleccionado.curso;
+        pais.nombreyapellido = AlumnoSeleccionado.nombreyapellido;
       }
     });
     setData(dataNueva);
@@ -56,23 +94,19 @@ const ListaAlumnos = () => {
 
   const eliminar = () => {
     setData(
-      data.filter(
-        (pais) =>
-          pais.numerodeexpediente !== paisSeleccionado.numerodeexpediente
-      )
+      data.filter((pais) => pais.expediente !== AlumnoSeleccionado.expediente)
     );
     setModalEliminar(false);
   };
 
   const abrirModalInsertar = () => {
-    setPaisSeleccionado(null);
+    setAlumnoSeleccionado(null);
     setModalInsertar(true);
   };
 
   const insertar = () => {
-    var valorInsertar = paisSeleccionado;
-    valorInsertar.numerodeexpediente =
-      data[data.length - 1].numerodeexpediente + 1;
+    var valorInsertar = AlumnoSeleccionado;
+    valorInsertar.expediente = data[data.length - 1].expediente + 1;
     var dataNueva = data;
     dataNueva.push(valorInsertar);
     setData(dataNueva);
@@ -98,16 +132,16 @@ const ListaAlumnos = () => {
           <tr>
             <th>N° de Expediente</th>
             <th>Nombre y Apellio</th>
-            <th>Estado de Cuota</th>
+            <th>Curso</th>
             <th>Acciones</th>
           </tr>
         </thead>
         <tbody>
           {data.map((elemento) => (
             <tr>
-              <td>{elemento.numerodeexpediente}</td>
-              <td>{elemento.nombre}</td>
-              <td>{elemento.minutos}</td>
+              <td>{elemento.expediente}</td>
+              <td>{elemento.nombreyapellido}</td>
+              <td>{elemento.curso}</td>
               <td>
                 <button
                   className="btn btn-info"
@@ -139,44 +173,74 @@ const ListaAlumnos = () => {
       <Modal isOpen={modalEditar}>
         <ModalHeader>
           <div>
-            <h3>Editar País</h3>
+            <h3>Editar Perfil de Alumno</h3>
           </div>
         </ModalHeader>
         <ModalBody>
           <div className="form-group">
-            <label>numerodeexpediente</label>
+            <label>N° de Expediente</label>
             <input
               className="form-control"
               readOnly
               type="text"
-              name="numerodeexpediente"
-              value={paisSeleccionado && paisSeleccionado.numerodeexpediente}
+              name="expediente"
+              value={AlumnoSeleccionado && AlumnoSeleccionado.expediente}
             />
             <br />
 
-            <label>País</label>
+            <label>Nombre y Apellido</label>
             <input
               className="form-control"
               type="text"
-              name="nombre"
-              value={paisSeleccionado && paisSeleccionado.nombre}
+              name="nombreyapellido"
+              value={AlumnoSeleccionado && AlumnoSeleccionado.nombreyapellido}
               onChange={handleChange}
             />
             <br />
 
-            <label>Minutos</label>
+            <label>Curso</label>
             <input
               className="form-control"
               type="text"
-              name="minutos"
-              value={paisSeleccionado && paisSeleccionado.minutos}
+              name="curso"
+              value={AlumnoSeleccionado && AlumnoSeleccionado.curso}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Domicilio</label>
+            <input
+              className="form-control"
+              type="text"
+              name="domicilio"
+              value={AlumnoSeleccionado && AlumnoSeleccionado.domicilio}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Contacto</label>
+            <input
+              className="form-control"
+              type="text"
+              name="contacto"
+              value={AlumnoSeleccionado && AlumnoSeleccionado.contacto}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>DNI</label>
+            <input
+              className="form-control"
+              type="text"
+              name="dni"
+              value={AlumnoSeleccionado && AlumnoSeleccionado.dni}
               onChange={handleChange}
             />
             <br />
           </div>
         </ModalBody>
         <ModalFooter>
-          <button className="btn btn-primary" onClick={() => editar()}>
+          <button className="btn btn-success" onClick={() => editar()}>
             Actualizar
           </button>
           <button
@@ -190,18 +254,18 @@ const ListaAlumnos = () => {
 
       <Modal isOpen={modalEliminar}>
         <ModalBody>
-          Estás Seguro que deseas eliminar el país{" "}
-          {paisSeleccionado && paisSeleccionado.nombre}
+          Estás Seguro que deseas Dar de Baja al Alumno N°{" "}
+          {AlumnoSeleccionado && AlumnoSeleccionado.expediente}
         </ModalBody>
         <ModalFooter>
-          <button className="btn btn-danger" onClick={() => eliminar()}>
-            Sí
+          <button className="btn btn-warning" onClick={() => eliminar()}>
+            Aceptar
           </button>
           <button
-            className="btn btn-secondary"
+            className="btn btn-danger"
             onClick={() => setModalEliminar(false)}
           >
-            No
+            Cancelar
           </button>
         </ModalFooter>
       </Modal>
@@ -209,45 +273,77 @@ const ListaAlumnos = () => {
       <Modal isOpen={modalInsertar}>
         <ModalHeader>
           <div>
-            <h3>Insertar País</h3>
+            <h3>Nuevo Perfil de Alumno</h3>
           </div>
         </ModalHeader>
         <ModalBody>
           <div className="form-group">
-            <label>numerodeexpediente</label>
+            <label>N° de Expediente</label>
             <input
               className="form-control"
               readOnly
               type="text"
-              name="numerodeexpediente"
-              value={data[data.length - 1].numerodeexpediente + 1}
+              name="expediente"
+              value={data[data.length - 1].expediente + 1}
             />
             <br />
 
-            <label>País</label>
+            <label>Nombre y Apellido</label>
             <input
               className="form-control"
               type="text"
-              name="nombre"
-              value={paisSeleccionado ? paisSeleccionado.nombre : ""}
+              name="nombreyapellido"
+              value={
+                AlumnoSeleccionado ? AlumnoSeleccionado.nombreyapellido : ""
+              }
               onChange={handleChange}
             />
             <br />
 
-            <label>Minutos</label>
+            <label>Curso</label>
             <input
               className="form-control"
               type="text"
-              name="minutos"
-              value={paisSeleccionado ? paisSeleccionado.minutos : ""}
+              name="curso"
+              value={AlumnoSeleccionado ? AlumnoSeleccionado.curso : ""}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Domicilio</label>
+            <input
+              className="form-control"
+              type="text"
+              name="domicilio"
+              value={AlumnoSeleccionado && AlumnoSeleccionado.domicilio}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Contacto</label>
+            <input
+              className="form-control"
+              type="text"
+              name="contacto"
+              value={AlumnoSeleccionado && AlumnoSeleccionado.contacto}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>DNI</label>
+            <input
+              className="form-control"
+              type="text"
+              name="dni"
+              value={AlumnoSeleccionado && AlumnoSeleccionado.dni}
               onChange={handleChange}
             />
             <br />
           </div>
         </ModalBody>
         <ModalFooter>
-          <button className="btn btn-primary" onClick={() => insertar()}>
-            Insertar
+          <button className="btn btn-success" onClick={() => insertar()}>
+            Crear
           </button>
           <button
             className="btn btn-danger"
