bc3c594b550b5aa80a62ee7b8d213467d6899c11
Rekke-uk <mazcm2013@gmail.com>
2021-06-18 15:41:26 -0300

    modal editar como componente

--- src/components/ModalEditar.jsx
+++ src/components/ModalEditar.jsx
@@ -1,95 +1,90 @@
-// import React from "react";
-// import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
-// import "../css/listaDeAlumnos.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "font-awesome/css/font-awesome.css";
-
-// const ModalEditar = () => {
-//   return (
-//     <>
-//       <Modal isOpen={modalEditar}>
-//         <ModalHeader>
-//           <div>
-//             <h3>Editar Perfil de Alumno</h3>
-//           </div>
-//         </ModalHeader>
-//         <ModalBody>
-//           <div className="form-group">
-//             <label>N° de Expediente</label>
-//             <input
-//               className="form-control"
-//               readOnly
-//               type="text"
-//               name="expediente"
-//               value={AlumnoSeleccionado && AlumnoSeleccionado.expediente}
-//             />
-//             <br />
-
-//             <label>Nombre y Apellido</label>
-//             <input
-//               className="form-control"
-//               type="text"
-//               name="nombreyapellido"
-//               value={AlumnoSeleccionado && AlumnoSeleccionado.nombreyapellido}
-//               onChange={handleChange}
-//             />
-//             <br />
-
-//             <label>Curso</label>
-//             <input
-//               className="form-control"
-//               type="text"
-//               name="curso"
-//               value={AlumnoSeleccionado && AlumnoSeleccionado.curso}
-//               onChange={handleChange}
-//             />
-//             <br />
-
-//             <label>Domicilio</label>
-//             <input
-//               className="form-control"
-//               type="text"
-//               name="domicilio"
-//               value={AlumnoSeleccionado && AlumnoSeleccionado.domicilio}
-//               onChange={handleChange}
-//             />
-//             <br />
-
-//             <label>Contacto</label>
-//             <input
-//               className="form-control"
-//               type="text"
-//               name="contacto"
-//               value={AlumnoSeleccionado && AlumnoSeleccionado.contacto}
-//               onChange={handleChange}
-//             />
-//             <br />
-
-//             <label>DNI</label>
-//             <input
-//               className="form-control"
-//               type="text"
-//               name="dni"
-//               value={AlumnoSeleccionado && AlumnoSeleccionado.dni}
-//               onChange={handleChange}
-//             />
-//             <br />
-//           </div>
-//         </ModalBody>
-//         <ModalFooter>
-//           <button className="btn btn-success" onClick={() => editar()}>
-//             Actualizar
-//           </button>
-//           <button
-//             className="btn btn-danger"
-//             onClick={() => setModalEditar(false)}
-//           >
-//             Cancelar
-//           </button>
-//         </ModalFooter>
-//       </Modal>
-//     </>
-//   );
-// };
-
-// export default ModalEditar;
+import React from "react";
+import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
+import "../css/listaDeAlumnos.css";
+const ModalEditar = ({
+  modalEditar,
+  setModalEditar,
+  AlumnoSeleccionado,
+  handleChange,
+  editar,
+}) => {
+  return (
+    <Modal isOpen={modalEditar}>
+      <ModalHeader>
+        <div>
+          <h3>Editar Perfil de Alumno</h3>
+        </div>
+      </ModalHeader>
+      <ModalBody>
+        <div className="form-group">
+          <label>N° de Expediente</label>
+          <input
+            className="form-control"
+            readOnly
+            type="text"
+            name="expediente"
+            value={AlumnoSeleccionado && AlumnoSeleccionado.expediente}
+          />
+          <br />
+          <label>Nombre y Apellido</label>
+          <input
+            className="form-control"
+            type="text"
+            name="nombreyapellido"
+            value={AlumnoSeleccionado && AlumnoSeleccionado.nombreyapellido}
+            onChange={handleChange}
+          />
+          <br />
+          <label>Curso</label>
+          <input
+            className="form-control"
+            type="text"
+            name="curso"
+            value={AlumnoSeleccionado && AlumnoSeleccionado.curso}
+            onChange={handleChange}
+          />
+          <br />
+          <label>Domicilio</label>
+          <input
+            className="form-control"
+            type="text"
+            name="domicilio"
+            value={AlumnoSeleccionado && AlumnoSeleccionado.domicilio}
+            onChange={handleChange}
+          />
+          <br />
+          <label>Contacto</label>
+          <input
+            className="form-control"
+            type="text"
+            name="contacto"
+            value={AlumnoSeleccionado && AlumnoSeleccionado.contacto}
+            onChange={handleChange}
+          />
+          <br />
+          <label>DNI</label>
+          <input
+            className="form-control"
+            type="text"
+            name="dni"
+            value={AlumnoSeleccionado && AlumnoSeleccionado.dni}
+            onChange={handleChange}
+          />
+          <br />
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <button className="btn btn-success" onClick={() => editar()}>
+          Actualizar
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={() => setModalEditar(false)}
+        >
+          Cancelar
+        </button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+export default ModalEditar;
--- src/pages/ListaAlumnos.jsx
+++ src/pages/ListaAlumnos.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
+import ModalEditar from "../components/ModalEditar";
 import "../css/listaDeAlumnos.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
@@ -190,87 +191,13 @@ const ListaAlumnos = () => {
         </tbody>
       </table>
 
-      <Modal isOpen={modalEditar}>
-        <ModalHeader>
-          <div>
-            <h3>Editar Perfil de Alumno</h3>
-          </div>
-        </ModalHeader>
-        <ModalBody>
-          <div className="form-group">
-            <label>N° de Expediente</label>
-            <input
-              className="form-control"
-              readOnly
-              type="text"
-              name="expediente"
-              value={AlumnoSeleccionado && AlumnoSeleccionado.expediente}
-            />
-            <br />
-
-            <label>Nombre y Apellido</label>
-            <input
-              className="form-control"
-              type="text"
-              name="nombreyapellido"
-              value={AlumnoSeleccionado && AlumnoSeleccionado.nombreyapellido}
-              onChange={handleChange}
-            />
-            <br />
-
-            <label>Curso</label>
-            <input
-              className="form-control"
-              type="text"
-              name="curso"
-              value={AlumnoSeleccionado && AlumnoSeleccionado.curso}
-              onChange={handleChange}
-            />
-            <br />
-
-            <label>Domicilio</label>
-            <input
-              className="form-control"
-              type="text"
-              name="domicilio"
-              value={AlumnoSeleccionado && AlumnoSeleccionado.domicilio}
-              onChange={handleChange}
-            />
-            <br />
-
-            <label>Contacto</label>
-            <input
-              className="form-control"
-              type="text"
-              name="contacto"
-              value={AlumnoSeleccionado && AlumnoSeleccionado.contacto}
-              onChange={handleChange}
-            />
-            <br />
-
-            <label>DNI</label>
-            <input
-              className="form-control"
-              type="text"
-              name="dni"
-              value={AlumnoSeleccionado && AlumnoSeleccionado.dni}
-              onChange={handleChange}
-            />
-            <br />
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <button className="btn btn-success" onClick={() => editar()}>
-            Actualizar
-          </button>
-          <button
-            className="btn btn-danger"
-            onClick={() => setModalEditar(false)}
-          >
-            Cancelar
-          </button>
-        </ModalFooter>
-      </Modal>
+      <ModalEditar
+        modalEditar={modalEditar}
+        setModalEditar={setModalEditar}
+        AlumnoSeleccionado={AlumnoSeleccionado}
+        handleChange={handleChange}
+        editar={editar}
+      />
 
       <Modal isOpen={modalEliminar}>
         <ModalBody>
