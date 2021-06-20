23cda9f9078cd4f0714461a8da41ca7608978127
Gabo <gabegarcia916@gmail.com>
2021-06-19 22:29:26 -0300

    Cree ruta cursos ruta alumnos

--- src/helpers/rutaAlumnos.js
+++ src/helpers/rutaAlumnos.js
@@ -1,11 +1,129 @@
-import React from 'react'
+import axios from "axios";
+import qs from "qs";
 
-const rutaAlumnos = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+//Traer todos los cursos con el limite y desde que registro
+export const getAlumnos = async (desde = 0, limite = 10) => {
+  let url = `http://localhost:3005/alumnos`;
 
-export default rutaAlumnos
+  const options = {
+    method: "GET",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  };
+  try {
+    const resp = await axios(url, options);
+    const { data } = resp;
+
+    return data;
+  } catch (error) {
+    return {
+      data: error.response.data,
+      loading: false,
+    };
+  }
+};
+
+//Traer un curso según su id
+export const getCursoId = async (id) => {
+  let url = `http://localhost:3005/cursos/${id}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  };
+  try {
+    const resp = await axios(url, options);
+    const { data } = resp;
+    console.log(data);
+    return data;
+  } catch (error) {
+    return {
+      data: error.response.data,
+      loading: false,
+    };
+  }
+};
+
+//Crear nuevo alumno
+export const addCurso = async (datos) => {
+  console.log(datos);
+  const token = JSON.parse(localStorage.getItem("token")) || "";
+  let url = "http://localhost:3005/alumno";
+
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      token: token,
+    },
+    data: qs.stringify(datos),
+  };
+  try {
+    const resp = await axios(url, options);
+    const { data } = resp;
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error.response.data);
+    return {
+      data: error.response.data,
+      loading: false,
+    };
+  }
+};
+
+//Actualizar curso
+export const modifAlumno = async (datos, id) => {
+  console.log(datos);
+  const token = JSON.parse(localStorage.getItem("token")) || "";
+  let url = `http://localhost:3005/alumno/${id}`;
+
+  const options = {
+    method: "PUT",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      token: token,
+    },
+    data: qs.stringify(datos),
+  };
+  try {
+    const resp = await axios(url, options);
+    const { data } = resp;
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error.response.data);
+    return {
+      data: error.response.data,
+      loading: false,
+    };
+  }
+};
+
+//Inactivar un curso
+export const delAlumno = async (id) => {
+  const token = JSON.parse(localStorage.getItem("token")) || "";
+  let url = `http://localhost:3004/alumno/${id}`;
+
+  const options = {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      token: token,
+    },
+  };
+  try {
+    const resp = await axios(url, options);
+    const { data } = resp;
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error.response.data);
+    return {
+      data: error.response.data,
+      loading: false,
+    };
+  }
+};
